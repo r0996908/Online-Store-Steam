20f8fe3744e31b529d0d89796a24a9df7196941c
r0996908 <r0996908@student.thomasmore.be>
2025-10-05 20:45:46 +0200

    feat: added login, password and submit labels on login page

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <head>
+        <div>
+            <form action="" method="$_POST">
+                <h2>Login</h2>
+
+                <div class= "fillbox">
+                    <label for="Email">E-mail</label>
+                    <input type="email" name="Email" id=""> 
+                </div>
+
+                <div class= "fillbox">
+                    <label for="Password">Wachtwoord</label>
+                    <input type="password" name="Password" id="">
+                </div>
+
+                <div class= "fillbox">
+                    <input type="submit" value="Log In">
+                </div>
+            </form>
+        </div>
+    </head>
+</body>
+</html>
