4fde562512b040a48fa6732c00d456eee254871f
r0996908 <r0996908@student.thomasmore.be>
2025-12-07 23:17:40 +0100

    fix: update test combell

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ if (!empty($_POST)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Test</title>
 </head>
 <body>
     <head>
